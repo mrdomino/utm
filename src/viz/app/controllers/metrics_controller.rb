eb00123993f6c03fb719e407a4861b035a31c594
Josh Lee <jleedev@gmail.com>
2009-04-19 21:12:00 -0400

    Chopped off some decimals to make gcharts happy

--- src/viz/app/controllers/metrics_controller.rb
+++ src/viz/app/controllers/metrics_controller.rb
@@ -8,8 +8,8 @@ end
 
 def generate_line_chart(dataset, title, axes)
   datapoints = dataset.collect {|h| h = h.collect{|k,v| v}}
-  datax = datapoints.collect {|x| x[0].to_f}
-  datay = datapoints.collect {|x| x[1].to_f}
+  datax = datapoints.collect {|x| x[0].to_f.to_i}
+  datay = datapoints.collect {|x| x[1].to_f.to_i}
   maxx = datax.max
   maxy = datay.max
   "http://chart.apis.google.com/chart?chs=300x200&cht=lxy&chd=t:#{datax.join(",")}|#{datay.join(",")}&chds=0,#{maxx.ceil},0,#{maxy.ceil}&chtt=#{title}&chxt=x,x,y,y&chxl=0:|0|#{maxx.ceil}|1:|#{axes[0]}|2:|0|#{maxy.ceil}|3:|#{axes[1]}&chxp=1,50|3,50"
